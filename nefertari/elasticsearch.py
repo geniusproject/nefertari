269dcee3dc0b569e1b813a2ede4201bf0f31cfcf
nikitagromov <nikita.gromov@ardas.ua>
2017-05-22 21:42:05 +0300

    Update timeout value

--- nefertari/elasticsearch.py
+++ nefertari/elasticsearch.py
@@ -321,7 +321,7 @@ class ESAction(metaclass=BoundAction):
 
     def __call__(self, *args, **kwargs):
         try:
-            self.params['timeout'] = 60
+            self.params['timeout'] = '60s'
             executed_num, errors = helpers.bulk(**self.params)
             log.info('Successfully executed {} Elasticsearch action(s)'.format(
                 executed_num))
